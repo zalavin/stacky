516c8a3b0107b06250a3742be506e44e91773783
Yan Zalavin <yztrnc@gmail.com>
2017-05-23 17:54:34 +0300

    Add repository for links

---
+++ stacky/repository.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Repository
+  SET_NAME = 'stacky:links'
+
+  def add(item)
+    redis.sadd set_name, item
+  end
+
+  def present?(item)
+    redis.sismember set_name, item
+  end
+
+  private
+
+  def redis
+    Redis.current
+  end
+
+  def set_name
+  end
+end
